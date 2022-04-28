94b77500c7a1da9bf8d8dce0b6a87d6ccaf4e15f
Carlos <cddd3871@gmail.com>
2022-04-28 12:21:16 -0400

    deply

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-
 require('./db.js');
 
 const server = express();
@@ -11,7 +10,6 @@ const server = express();
 server.name = 'API';
 
 
-server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
