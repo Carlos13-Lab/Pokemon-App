ee3258842628aeae6e9d313786f9522882f3763e
Carlos Diaz <Carlosrdf45@gmai.com>
2025-06-04 19:14:19 -0400

    Main Server.js Bug

--- api/src/server.js
+++ api/src/server.js
@@ -2,48 +2,46 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const {
-  Pokemon,
-} = require('./routes/routes');
 const cors = require('cors');
-require('./database/config');
-const PokemonRepository = require("./Repository/pokemon.repository.js");
-
-
+require('dotenv').config();
+const { conn } = require('./database/config.js'); // Importar la conexión a la base de datos
+const { Pokemon } = require('./routes/routes');
 
 const server = express();
-const pokemonRepository = new PokemonRepository();
 server.name = 'API';
 
-server.use(cors())
+// Configuración de middlewares
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // Actualiza esto según tu dominio
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
-//Llamar al método para popular la base de datos una sola vez
-  (async () => {
-    try {
-      await pokemonRepository.populateTypesOnce();
-    } catch (error) {
-      console.error("Error al inicializar la base de datos:", error.message);
-    }
-  })();
+// Llamar al método para popular la base de datos una sola vez
+
+// Rutas
 server.use('/api/pokemon', Pokemon);
 
-// Error catching endware.
+// Error catching endware
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
+});
+
+// Sincronizar los modelos y arrancar el servidor
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at port " + process.env.PORT); // eslint-disable-line no-console
+  });
 });
 
 module.exports = server;
