2ff639c011107a9b08f87ff52e3576575e348f91
Carlos Diaz <Carlosrdf45@gmai.com>
2025-06-10 11:49:01 -0400

    UPDATE API CLIENt

--- api/src/server.js
+++ api/src/server.js
@@ -17,7 +17,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pokemonclient-9jxtqutkd-carlos13labs-projects.vercel.app'); // Actualiza esto según tu dominio
+  res.header('Access-Control-Allow-Origin', 'pokemonclient-dun.vercel.app'); // Actualiza esto según tu dominio
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
